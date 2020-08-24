43af9be88adf98ee0a4825a45e76e1450cdb4764
Philip Johnson <philip.johnson.it@gmail.com>
2020-08-24 09:30:37 -0400

    step 3 is a buste

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Add validation!');
+
+// var re = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
+
+// function testInfo(phoneInput) {
+//     var OK = re.exec(phoneInput.value);
+//     if (!OK) {
+//         console.error(phoneInput.value + ' isn\'t a phone number with area code!');
+//     } else {
+//         console.log('Thanks, your phone number is ' + OK[0]);
+//     }
+// }
